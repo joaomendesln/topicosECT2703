7b27f30a5c58f3a0dc8458c5bd6290939bbdd745
João Mendes Lopes Neto <joaomendes_ln@MacBook-Pro-de-Joao.local>
2021-01-31 18:49:30 -0300

    Correção no exercício 01.2 da aula 03

--- Aula 03/1_casa.hs
+++ Aula 03/1_casa.hs
@@ -1,10 +1,7 @@
 primChar :: [Char] -> Bool
 primChar (h:t) = elem h t
 
--- iguaisIniFim :: (Num n) => n -> [n] -> Bool
--- iguaisIniFim k l = take k l == drop (length l - k) l
-
-iguaisIniFim :: Int -> [Int] -> Bool
+iguaisIniFim :: (Eq n) => Int -> [n] -> Bool
 iguaisIniFim n l = take n l == drop (length l - n) l
 
 ehMaiuscula :: Char -> Bool
