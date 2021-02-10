ec7f3785784028b452e684834719d2cea70fa73e
João Mendes Lopes Neto <joaomendes_ln@MacBook-Pro-de-Joao.local>
2021-02-10 19:38:32 -0300

    Correcão no exercício 2 da aula 05

--- Aula 05/02.hs
+++ Aula 05/02.hs
@@ -1,11 +1,11 @@
-compararDatas :: (Int, Int, Int) -> (Int, Int, Int) -> Int
+type Data = (Int, Int, Int)
+
+compararDatas :: (Data) -> (Data) -> Int
 compararDatas (d1, m1, a1) (d2, m2, a2)
  | a1 <  a2 = -1
  | a2 >  a1 = 1
- | a1 == a2 =
-     | m1 <  m2 = -1
-     | m2 <  m1 = 1
-     | m1 == m2 =
-        | d1 <  d2 = -1
-	 	| d2 <  d1 = 1
-	 	| d1 == d2 = 0
+ | m1 <  m2 = -1
+ | m2 <  m1 = 1
+ | d1 <  d2 = -1
+ | d2 <  d1 = 1
+ | d1 == d2 = 0
